dc1e62ec853c4e4350bc158b0ca2c29520e46a9b
Matthew Szeto <ms8288@gmail.com>
2019-05-03 11:42:12 -0700

    added ItemQuestion

---
+++ CECS453FinalProject/app/src/main/java/com/example/cecs453finalproject/Model/ItemQuestion.java
@@ -0,0 +1,11 @@
+package com.example.cecs453finalproject.Model;
+
+public class ItemQuestion {
+    private String question;
+    private String answer;
+
+    public ItemQuestion(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
