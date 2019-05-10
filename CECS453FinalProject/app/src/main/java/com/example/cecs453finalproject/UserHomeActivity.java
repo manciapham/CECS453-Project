6e5ecd6c98cf6824c4fec6a46d3dbfc0a861d636
Matthew Szeto <ms8288@gmail.com>
2019-05-10 11:13:24 -0700

    add nav bar

--- CECS453FinalProject/app/src/main/java/com/example/cecs453finalproject/UserHomeActivity.java
+++ CECS453FinalProject/app/src/main/java/com/example/cecs453finalproject/UserHomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.cecs453finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  *  User Homepage
@@ -12,9 +15,22 @@ import android.os.Bundle;
  */
 
 public class UserHomeActivity extends AppCompatActivity {
+
+    private Button navDrawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+        navDrawer = (Button)findViewById(R.id.btn_nav_drawer);
+
+        navDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserHomeActivity.this, NavigationDrawerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
