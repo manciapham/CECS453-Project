a29a7148e80dc8208c7b537985f4f897ea7c32c6
Dominique Oyco <dominickoyco@gmail.com>
2019-05-10 10:11:54 -0700

    update UI

---
+++ CECS453FinalProject/app/src/main/java/com/example/cecs453finalproject/ChangeCredentialActivity.java
@@ -0,0 +1,13 @@
+package com.example.cecs453finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangeCredentialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_credential);
+    }
+}
