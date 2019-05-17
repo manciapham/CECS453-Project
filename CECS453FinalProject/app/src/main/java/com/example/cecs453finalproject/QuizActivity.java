19ac209a6779e5fb3d6505f9cee833bef5653bce
Mancia Pham <manciapham@gmail.com>
2019-05-16 18:19:08 -0700

    Quiz activity and grade quiz activity

--- CECS453FinalProject/app/src/main/java/com/example/cecs453finalproject/QuizActivity.java
+++ CECS453FinalProject/app/src/main/java/com/example/cecs453finalproject/QuizActivity.java
@@ -1,19 +1,17 @@
 package com.example.cecs453finalproject;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.example.cecs453finalproject.Model.Question;
+//import com.example.quizzical.SQL.DatabaseHelper2;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,106 +22,134 @@ import java.util.Collections;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private TextView question0;
-    private EditText answer0;
-    private TextView question1;
-    private EditText answer1;
-    private TextView question2;
-    private EditText answer2;
-    private TextView question3;
-    private EditText answer3;
-    private TextView question4;
-    private EditText answer4;
-    private TextView question5;
-    private EditText answer5;
-    private TextView question6;
-    private EditText answer6;
-    private TextView question7;
-    private EditText answer7;
-    private TextView question8;
-    private EditText answer8;
-    private TextView question9;
-    private EditText answer9;
-    private Button quizSubmit;
+    private static final String KEY_SCORE = "keyScore";
+    private static final String KEY_QUESTION_COUNT = "keyQuestionCount";
+    private static final String KEY_ANSWERED = "keyAnswered";
+    private static final String KEY_QUESTION_LIST = "keyQuestionList";
 
-    private static final String TAG = "QuizActivity";
-    ArrayList<String> uQ = new ArrayList<>(); // array of questions
-    ArrayList<String> uA = new ArrayList<>(); // array of answers
+    private TextView textViewQuestion;
+    private TextView textViewQuestionCount;
+    private RadioGroup rbGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private RadioButton rb4;
+    private Button buttonNext;
+
+    private ArrayList<Question> questionList;
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+    public static int score;
+
+    private Boolean answered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        //submit quiz button
-        quizSubmit = (Button) findViewById(R.id.quizButton);
+        score = 0;
 
-        downloadJSON("https://nisalgamage.com/qNa");
+        textViewQuestion = findViewById(R.id.text_view_question);
+        textViewQuestionCount = findViewById(R.id.text_view_question_count);
+        rbGroup = findViewById(R.id.radio_group);
+        rb1 = findViewById(R.id.radio_button1);
+        rb2 = findViewById(R.id.radio_button2);
+        rb3 = findViewById(R.id.radio_button3);
+        rb4 = findViewById(R.id.radio_button4);
+        buttonNext = findViewById(R.id.button_next);
 
-        ArrayList<Integer> order = new ArrayList<>(); //order of questions
-        for(int i = 0; i < uQ.size(); i++ ) {
-            order.add(i);
+        if (savedInstanceState == null) {
+            DatabaseHelper2 databaseHelper2 = new DatabaseHelper2(this);
+            questionList = databaseHelper2.getAllQuestion();
+            questionCountTotal = questionList.size();
+            Collections.shuffle(questionList);
+
+            showNextQuestion();
+        } else {
+            questionList = savedInstanceState.getParcelableArrayList(KEY_QUESTION_LIST);
+            if (questionList == null) {
+                showScore();
+            }
+            questionCountTotal = questionList.size();
+            questionCounter = savedInstanceState.getInt(KEY_QUESTION_COUNT);
+            currentQuestion = questionList.get(questionCounter - 1);
+            score = savedInstanceState.getInt(KEY_SCORE);
+            answered = savedInstanceState.getBoolean(KEY_ANSWERED);
         }
-        Collections.shuffle(order);
 
-        question0.setText("1. " + uQ.get(order.get(0)));
-        question1.setText("2. " + uQ.get(order.get(1)));
-        question2.setText("3. " + uQ.get(order.get(2)));
-        question3.setText("4. " + uQ.get(order.get(3)));
-        question4.setText("5. " + uQ.get(order.get(4)));
-        question5.setText("6. " + uQ.get(order.get(5)));
-        question6.setText("7. " + uQ.get(order.get(6)));
-        question7.setText("8. " + uQ.get(order.get(7)));
-        question8.setText("9. " + uQ.get(order.get(8)));
-        question9.setText("10. " + uQ.get(order.get(9)));
-
-        quizSubmit.setOnClickListener(new View.OnClickListener() {
+        buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //TODO: submit quiz & grade
+            public void onClick(View v) {
+                if (!answered) {
+                    if (rb1.isChecked() || rb2.isChecked() || rb3.isChecked() || rb4.isChecked()) {
+                        checkAnswer();
+                    } else {
+                        Toast.makeText(QuizActivity.this, "Please select an answer", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    if (questionCounter < questionCountTotal) {
+                        buttonNext.setText("Next");
+                    } else {
+                        buttonNext.setText("Finish");
+                    }
+                    showNextQuestion();
+                }
             }
         });
     }
 
-    ////
-    private void downloadJSON(final String urlWebService) {
-        class DownloadJSON extends AsyncTask<Void, Void, String> {
-            @Override
-            protected String doInBackground(Void... voids) {
+    private void showNextQuestion() {
+        if (questionCounter < questionCountTotal) {
+            currentQuestion = questionList.get(questionCounter);
 
-                HttpHandler sh = new HttpHandler();
-                String jsonStr = sh.makeServiceCall(urlWebService);
-                Log.e(TAG, "response" + jsonStr);
-                if (jsonStr != null) {
-                    try {
-                        JSONObject jsonObject = new JSONObject(jsonStr);
-                        JSONArray users = jsonObject.getJSONArray("dataQNA");
+            textViewQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getAnswer1());
+            rb2.setText(currentQuestion.getAnswer2());
+            rb3.setText(currentQuestion.getAnswer3());
+            rb4.setText(currentQuestion.getAnswer4());
 
-                        for (int i = 0; i < users.length(); i++) {
-                            JSONObject un = users.getJSONObject(i);
-                            String quest = un.getString("question");
-                            String ans = un.getString("answer");
+            questionCounter++;
+            textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
+            answered = false;
 
-                            uQ.add(quest);
-                            uA.add(ans);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return null;
-            }
-
-            protected void onPreExecute() {
-                super.onPreExecute();
-            }
+        } else {
+            showScore();
         }
-
-        DownloadJSON getJSON = new DownloadJSON();
-        getJSON.execute();
     }
 
-    private void gradeQuestion(final String userAnswer, final String correctAnswer) {
+    private void checkAnswer() {
+        answered = true;
 
+        RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
+        int answer = rbGroup.indexOfChild(rbSelected) + 1;
+
+        if ( answer == Integer.parseInt(currentQuestion.getCorrect())) {
+            score++;
+        }
+    }
+
+    public static int getScore(){
+        return score;
+    }
+
+    private void showScore() {
+        Intent intent = new Intent(QuizActivity.this, GradeQuizActivity.class);
+        startActivity(intent);
+        finishQuiz();
+    }
+
+    private void finishQuiz() {
+        finish();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_SCORE, score);
+        outState.putInt(KEY_QUESTION_COUNT, questionCounter);
+        outState.putBoolean(KEY_ANSWERED, answered);
+        outState.putParcelableArrayList(KEY_QUESTION_LIST, questionList);
     }
 }
